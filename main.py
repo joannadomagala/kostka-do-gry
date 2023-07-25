60cdeb7ef4fdbb7a8bf5f849b6d9915b10b39e56
joannadomagala <127305810+joannadomagala@users.noreply.github.com>
2023-07-25 21:39:40 +0200

    pause

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+def on_button_pressed_a():
+    basic.show_icon(IconNames.HEART)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    basic.clear_screen()
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
 def on_gesture_shake():
     basic.show_number(randint(1, 6))
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
-basic.show_icon(IconNames.HEART)
+basic.show_string("Hi!")
+basic.clear_screen()
